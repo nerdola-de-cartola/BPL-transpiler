1ebf39ec80681ec035c94326a2b1978e3b5d8823
Arthur Emanuel <arthur-er@protonmail.com>
2023-02-01 11:33:41 -0300

    feat: documentação

--- include/array.h
+++ include/array.h
@@ -6,12 +6,31 @@
 #include "registers.h"
 #include "functions.h"
 
+/**
+  * @brief Verifica se o acesso ao array é válido
+  * @param r Número de parâmetros lidos (sscanf)
+  * @param c1 primeiro  parametro ou vetor
+  * @param c2 parametro, vetor ou variavel
+  * @param c3 valor a ser atribuido
+  */
 void verifyArrayAccess(int r, char c1, char c2, char c3);
 
 void arrayAccess();
 
+/**
+  * @brief Realiza o acesso ao array
+  * @param r Ponteiro para o registrador que será utilizado
+  * @param type Se vai enviar para uma variavel ou para um parametro
+  * @param index Índice a ser acessado
+  */
 void arrayAccessGet(Register *r, char type, int index);
 
+/**
+  * @brief Realiza a atribuição ao array
+  * @param r Ponteiro para o registrador que será utilizado
+  * @param type Se vai enviar para uma variavel ou para um parametro
+  * @param index Índice a ser definido
+  */
 void arrayAccessSet(Register *r, char type, int index);
 
 #endif
--- include/functions.h
+++ include/functions.h
@@ -27,24 +27,56 @@ typedef struct {
 extern Function FUNCTIONS[MAX_FUNCTION];
 extern int CURRENT_FUNCTION_INDEX;
 
+/**
+ * @brief Inicializa o vetor de funções
+*/
 void functionsInit();
 
+/**
+ * @brief Cria uma função por completo
+*/
 void functionDefinition();
 
+/**
+ * @brief Verifica se os parâmetros passados para uma chamada de função são válidos
+ * @param index Índice da função
+ * @param qtd Quantidade de parâmetros
+ * @param type_destiny Tipo do destino do retorno da função (variável ou parâmetro)
+ * @param category Categoria do destino do retorno da função (constante, variavel ou parâmetro)
+ * @param type Tipo do valor na categoria (inteiro ou vetor)
+*/
 int verifyCallFunction(int qtd, char type_destiny, char category[3], char type[3]);
 
+/**
+ * @brief Realiza a chamada de uma função
+*/
 void callFunction();
 
+/**
+ * @brief Verifica se a função retorna um tipo valido
+*/
 void verifyReturn(char type);
 
+/**
+ * @brief Move o valor de retorno para os registradores (em assembly)
+*/
 void returnFunction();
 
+/**
+ * @brief imprime a subtração da pilha de uma função (em assembly)
+*/
 void subq(int lastStackPos);
 
 Function *getFunction(int index);
 
+/**
+ * @brief Imprime o cabeçalho de uma função (em assembly)
+*/
 void printFunctionHeader();
 
+/**
+ * @brief Imprime a finalização de uma função (em assembly)
+*/
 void functionEnd();
 
 #endif
